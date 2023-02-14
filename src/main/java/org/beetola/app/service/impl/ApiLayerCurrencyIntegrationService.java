92affcec99280c436752499ea28b790f3be77036
romajan <roma.grigoriev.2015@inbox.ru>
2023-02-14 23:03:27 +0400

    Added normal validation of api response

--- src/main/java/org/beetola/app/service/impl/ApiLayerCurrencyIntegrationService.java
+++ src/main/java/org/beetola/app/service/impl/ApiLayerCurrencyIntegrationService.java
@@ -2,17 +2,21 @@ package org.beetola.app.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.beetola.app.exception.CurrencyException;
 import org.beetola.app.exception.WebException;
 import org.beetola.app.model.dto.CurrencyRateRs;
 import org.beetola.app.service.CurrencyIntegrationService;
+import org.springframework.http.HttpStatus;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+import static java.util.Objects.nonNull;
 import static java.util.Optional.ofNullable;
 
 @Slf4j
@@ -32,7 +36,7 @@ public class ApiLayerCurrencyIntegrationService implements CurrencyIntegrationSe
                         .build())
                 .retrieve()
                 .onStatus(httpStatus ->
-                        httpStatus.value() > 200, clientResponse ->
+                        httpStatus.value() > HttpStatus.OK.value(), clientResponse ->
                         Mono.error(new WebException("ApiLayer returned bad response",
                                 clientResponse.statusCode(), clientResponse.statusCode().getReasonPhrase())))
                 .bodyToMono(CurrencyRateRs.class)
@@ -42,10 +46,26 @@ public class ApiLayerCurrencyIntegrationService implements CurrencyIntegrationSe
 
     private static Map<String, String> validatedResponse(CurrencyRateRs rs) {
         return ofNullable(rs)
-                .map(CurrencyRateRs::getQuotes)
-                .orElseGet(() -> {
-                    log.warn("Response from currency rs is null");
-                    return new HashMap<>();
-                });
+                .map(response -> {
+                    Boolean isSuccess = ofNullable(response.getSuccess())
+                            .orElseThrow(() ->
+                                    new CurrencyException("Status from response is null!"));
+                    if (!isSuccess) {
+                        throw new CurrencyException("Status from response is not success!");
+                    }
+                    Map<String, String> filtered = response.getQuotes()
+                            .entrySet().stream()
+                            .filter(e -> nonNull(e.getKey()) &&
+                                    e.getKey().length() == 6 &&
+                                    NumberUtils.isCreatable(e.getValue()))
+                            .collect(Collectors.toMap(
+                                    Map.Entry::getKey,
+                                    Map.Entry::getValue
+                            ));
+                    if (filtered.isEmpty()) {
+                        throw new CurrencyException("Values from response are invalid!");
+                    }
+                    return filtered;
+                }).orElseThrow(() -> new CurrencyException("Response body is empty!"));
     }
 }
