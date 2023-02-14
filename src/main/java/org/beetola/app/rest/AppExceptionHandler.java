7af7b95174117527a2388f9fc153acee84d5a022
romajan <roma.grigoriev.2015@inbox.ru>
2023-02-14 23:03:46 +0400

    Added app exception handler

---
+++ src/main/java/org/beetola/app/rest/AppExceptionHandler.java
@@ -0,0 +1,35 @@
+package org.beetola.app.rest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.beetola.app.exception.CurrencyException;
+import org.beetola.app.model.dto.InvalidInputRs;
+import org.beetola.app.model.dto.ServerError;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.validation.ConstraintViolationException;
+
+@Slf4j
+@ControllerAdvice
+public class AppExceptionHandler {
+
+    @ExceptionHandler(CurrencyException.class)
+    public ResponseEntity<?> handle(CurrencyException ex) {
+        log.debug("Bad input: {}", ex.getMessage(), ex);
+        return ResponseEntity.badRequest().body(new InvalidInputRs().message(ex.getMessage()));
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<?> handleConstraintViolation(ConstraintViolationException ex) {
+        log.debug("Bad input: {}", ex.getMessage(), ex);
+        return ResponseEntity.badRequest().body(new InvalidInputRs().message("Bad input parameters!"));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleAppException(Exception ex) {
+        log.error("Application error : {}", ex.getMessage(), ex);
+        return ResponseEntity.internalServerError().body(new ServerError().message("Oops, smth went wrong :("));
+    }
+
+}
