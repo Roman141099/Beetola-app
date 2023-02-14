d685a7a2a2462483f1064e976a6864a38f868d76
romajan <roma.grigoriev.2015@inbox.ru>
2023-02-14 23:36:17 +0400

    Deleted PostConstruct

--- src/main/java/org/beetola/app/config/CurrencyConversionConfig.java
+++ src/main/java/org/beetola/app/config/CurrencyConversionConfig.java
@@ -5,8 +5,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
 
-import javax.annotation.PostConstruct;
-
 @Getter
 @Validated
 @Configuration
@@ -14,10 +12,5 @@ public class CurrencyConversionConfig {
 
     @Value("#{${app.margin-percent:2} / 100.0}")
     private Double marginPercent;
-
-    @PostConstruct
-    public void init() {
-        System.out.println();
-    }
 
 }
